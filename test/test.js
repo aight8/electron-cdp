e636041c72c6003a489819162f0d477ee1a09706
Sebastian Fekete <aight8@gmail.com>
2017-05-17 09:52:55 +0200

    fix(home): a small fix
    yes its a small fix
    
    BREAKING CHANGE: no breakings!
    
    234

--- test/test.js
+++ test/test.js
@@ -3,4 +3,4 @@ const CDP = require('../')
 console.log('something and so one... some fake feature...')
 console.log('added lots of features')
 console.log('this is now a BUG fix!')
-
+console.log('fix another important BUG!')
