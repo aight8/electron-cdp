a7224db2ba640585db7dfed98b5a39517594ae39
Sebastian Fekete <aight8@gmail.com>
2017-05-17 10:02:56 +0200

    fix(clean): clean test codes
    yes there was a lot

--- test/test.js
+++ test/test.js
@@ -1,6 +1,1 @@
 const CDP = require('../')
-
-console.log('something and so one... some fake feature...')
-console.log('added lots of features')
-console.log('this is now a BUG fix!')
-console.log('fix another important BUG!')
